729815cfe4a31bdcff9918c202b7e71bd39359e1
gaohuibin <gaohuibin15@163.com>
2021-11-15 10:10:02 +0800

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Author: gaohuibin
+ * Date: 2021/11/15 10:02
+ * Desc:
+ * Version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
